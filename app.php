74aea7c4a084d8ec4972b0d369a63b431e3fe702
sandesh2000 <sandeshjain174@gmail.com>
2023-07-20 23:56:04 +0530

    app.php v4 released using argocd

--- app.php
+++ app.php
@@ -2,5 +2,5 @@
         print "Your IP address is ".$_SERVER['REMOTE_ADDR'];
         echo "<body style='background-color:pink'>";
         echo "<br>";
-        echo "k3s cluster testing...Mindfire v2 : CI using Jenkins & CD using ArgoCD"
+        echo "k3s cluster testing...Mindfire v4 : CI using Jenkins & CD using ArgoCD"
 ?>
