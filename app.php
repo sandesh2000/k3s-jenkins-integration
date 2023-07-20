5c351ebce283ddbacee2ce346b72e813a6a618c9
sandesh2000 <sandeshjain174@gmail.com>
2023-07-20 13:42:44 +0530

    app.php updated

--- app.php
+++ app.php
@@ -2,5 +2,5 @@
         print "Your IP address is ".$_SERVER['REMOTE_ADDR'];
         echo "<body style='background-color:pink'>";
         echo "<br>";
-        echo "k3s cluster testing...From Jenkins"
+        echo "k3s cluster testing...Latest push : From Jenkins"
 ?>
