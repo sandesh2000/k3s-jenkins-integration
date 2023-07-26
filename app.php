61219775da776699e05eef9e9e58b8a88acb3bb1
sandesh2000 <sandeshjain174@gmail.com>
2023-07-27 00:33:40 +0530

    Product v1 & Image v1 launched

--- app.php
+++
@@ -1,6 +0,0 @@
-<?php
-        print "Your IP address is ".$_SERVER['REMOTE_ADDR'];
-        echo "<body style='background-color:pink'>";
-        echo "<br>";
-        echo "k3s cluster testing...Mindfire v13 : QA Testing"
-?>
