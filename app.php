8a3e0906cfab73100227e158a5b435f93b25b5fa
sandesh2000 <sandeshjain174@gmail.com>
2023-07-26 14:41:09 +0530

    New version v13

--- app.php
+++ app.php
@@ -2,5 +2,5 @@
         print "Your IP address is ".$_SERVER['REMOTE_ADDR'];
         echo "<body style='background-color:pink'>";
         echo "<br>";
-        echo "k3s cluster testing...Mindfire v12 : QA Testing"
+        echo "k3s cluster testing...Mindfire v13 : QA Testing"
 ?>
