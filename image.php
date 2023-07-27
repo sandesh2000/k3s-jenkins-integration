77069e529ad9580b8620f7447cb9282efe1f2ca8
sandesh2000 <sandeshjain174@gmail.com>
2023-07-27 10:25:09 +0530

    v2 image and product launched

--- image.php
+++ image.php
@@ -2,5 +2,5 @@
         print "Your IP address is ".$_SERVER['REMOTE_ADDR'];
         echo "<body style='background-color:pink'>";
                 echo "<br>";
-                echo "Images Page v1 launched"
+                echo "Images Page v2 launched"
 ?>
--- product.php
+++ product.php
@@ -2,5 +2,5 @@
         print "Your IP address is ".$_SERVER['REMOTE_ADDR'];
         echo "<body style='background-color:pink'>";
                 echo "<br>";
-                echo "Products Page v1 launched"
+                echo "Products Page v2 launched"
 ?>
